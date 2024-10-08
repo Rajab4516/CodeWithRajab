e301fce90848848b028996a09d9933cddb273696
Rajab Asghar <alfatahrays@gamil.com>
2024-10-08 04:45:04 -0700

    Add Buttons in the Text Form

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -3,15 +3,20 @@ import React from 'react'
 function TextForm(props) {
   return (
     <>
+      <div className="text-form-heading-container">
+        <h1>{props.heading}</h1>
+      </div>
       <div className="text-form-main-container">
-                <div className="text-form-heading-container">
-                    <h1>{props.heading}</h1>
-                </div>
             <div className="container">
                 <textarea name="" id="" cols="150" rows="15"></textarea>
-                <button>Convert To UpperCase</button>
             </div>
+      </div>
+      <div className="buttons-container">
+        <div className="buttons">
+          <button>Convert To UpperCase</button>
+          <button>Convert To LowerCase</button>
         </div>
+      </div>
     </>
   )
 }
